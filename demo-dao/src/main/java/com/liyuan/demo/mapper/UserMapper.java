f34fc7ed8021ffa2ca1a365663edf5d0ac88af12
liyuan <707335344@qq.com>
2018-05-11 14:17:13 +0800

    add：框架清空

--- demo-dao/src/main/java/com/liyuan/demo/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.liyuan.demo.mapper;
-
-import com.liyuan.demo.domain.po.user.UserPo;
-import com.liyuan.demo.domain.condition.user.UserCondition;
-import com.liyuan.demo.mapper.base.BaseMapper;
-
-public interface UserMapper extends BaseMapper<UserPo, UserCondition> {
-
-}
